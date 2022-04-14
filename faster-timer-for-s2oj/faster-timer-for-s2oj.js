7e1ae8dfc68c123e16c1416a31f19a00d785eb91
Xi'En <79648987+XiEn1847@users.noreply.github.com>
2022-04-14 17:28:41 +0800

    Create faster-timer-for-s2oj.js

---
+++ faster-timer-for-s2oj/faster-timer-for-s2oj.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         faster-timer-for-s2oj
+// @namespace    http://tampermonkey.net/
+// @version      1.1.4
+// @description  Time goes fas
+// @author       XiEn
+// @match        https://www.sjzezoj.com/*
+// @match        https://sjzezoj.com/*
+// @icon         https://sjzezoj.com/images/logo_small.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $('#t-2022sx').hide();
+    $('#t-2022sx').after("<b id=\"t-2022sx-fast\"></b>");
+    $('#t-2022noip').hide();
+    $('#t-2022noip').after("<b id=\"t-2022noip-fast\"></b>");
+ 
+    var sx2022 = new Date('2022-04-16').getTime();
+    var noip2022 = new Date('2022-11-19').getTime();
+    setInterval(() => {
+        var now = new Date().getTime();
+        $('#t-2022sx-fast').text(((sx2022 - now) / (24 * 60 * 60) / 1000).toFixed(8));
+        $('#t-2022noip-fast').text(((noip2022 - now) / (24 * 60 * 60) / 1000).toFixed(8));
+    }, 0);
+})();
