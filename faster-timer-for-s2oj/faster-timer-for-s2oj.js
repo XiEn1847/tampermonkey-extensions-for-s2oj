5161217c0597176a0565e8a4853046b50bb80744
Xi'En <79648987+XiEn1847@users.noreply.github.com>
2022-04-15 09:59:52 +0800

    Update faster-timer-for-s2oj.js

--- faster-timer-for-s2oj/faster-timer-for-s2oj.js
+++ faster-timer-for-s2oj/faster-timer-for-s2oj.js
@@ -2,7 +2,7 @@
 // @name         faster-timer-for-s2oj
 // @namespace    http://tampermonkey.net/
 // @version      1.1.4
-// @description  Time goes fas
+// @description  Time goes fast!
 // @author       XiEn
 // @match        https://www.sjzezoj.com/*
 // @match        https://sjzezoj.com/*
@@ -12,10 +12,15 @@
 
 (function() {
     'use strict';
-    $('#t-2022sx').hide();
-    $('#t-2022sx').after("<b id=\"t-2022sx-fast\"></b>");
-    $('#t-noip2022').hide();
-    $('#t-noip2022').after("<b id=\"t-2022noip-fast\"></b>");
+    $("div[class='col-xs-6 col-sm-4 col-md-3']:contains(倒计时)").children("center").hide()
+    $("div[class='col-xs-6 col-sm-4 col-md-3']:contains(倒计时)").append(
+        '<center><div>'+
+        '<h1>倒计时</h1>'+
+        '<br/>'+
+        '<p>距离 <b>2022 年省选</b> 还有 <b id="t-2022sx-fast">-1</b> 天</p>'+
+        '<p>距离 <b>NOIP 2022</b> 还有 <b id="t-2022noip-fast">-1</b> 天</p>'+
+        '</div></center>'
+    );
 
     var sx2022 = new Date('2022-04-16').getTime();
     var noip2022 = new Date('2022-11-19').getTime();
